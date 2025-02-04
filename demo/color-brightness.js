2432b067b69cbe6a66604a743ad0aacd212f01f9
Ulvi YELEN <ulvido@gmail.com>
2025-02-04 17:14:09 +0300

    FIX: color-brightness

--- demo/color-brightness.js
+++ demo/color-brightness.js
@@ -16,8 +16,8 @@ const brightnesses = [
 
 // -- ML5.js
 let options = {
-  inputs: ["red", "green", "blue"],
-  outputs: ["brightness"],
+  inputs: ["red", "green", "blue", "brightness"],
+  outputs: ["label"],
   task: "classification", // sonuç ne kadar C, ne kadar D, ne kadar E; 3ünün de ihtimalini verir (confidence ile). https://youtu.be/8HEgeAbYphA?list=PLRqwX-V7Uu6YPSwT06y_AEYTqIwbeam3y
   // task: "regression", // sonuç daha çok slider gibi C, D, E arasındaki bir değeri de verebilir. tüm inputları hesaba katan tek bir sonuç verir. rakam alır rakam verir. https://youtu.be/fFzvwdkzr_c?list=PLRqwX-V7Uu6YPSwT06y_AEYTqIwbeam3y
   debug: true,
@@ -30,12 +30,11 @@ let model = ml5.neuralNetwork(options);
 let data = [];
 
 for (let i = 0; i < colors.length; i++) {
-  data.push({
-    inputs: colors[i],
-    outputs: brightnesses[i],
-  })
+  const input = { ...colors[i], brightness: Object.values(brightnesses[i])[0] };
+  const output = Object.keys(brightnesses[i])[0];
+  data.push({ input, label: output });
 
-  model.addData(colors[i], Object.values(brightnesses)[i]);
+  model.addData(input, { label: output });
 }
 
 content.innerText += JSON.stringify({ data }, null, 2) + "\n";
@@ -48,11 +47,16 @@ let optionsTraining = {
   batchSize: 12,
 }
 
-const gotResult = (result) => { console.log(result) }
+const gotResult = (result) => {
+  content.innerText += JSON.stringify({ result }, null, 2) + "\n";
+  console.log("result: ", result[0].label);
+}
+
 const whileTraining = () => { }
+
 const finishedTraining = () => {
   console.log("training finished", model);
-  model.classify({ red: 0.9 }, gotResult)
+  model.classify({ red: 0.9, green: 0.0, blue: 0.0, brightness: 0.8 }, gotResult)
 }
 
 model.train(optionsTraining, whileTraining, finishedTraining);
